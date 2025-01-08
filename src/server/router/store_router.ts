2c8643ab2c4cfaaf421a91deba187552617969bc
Marco Bizzaro <51394894+snqre@users.noreply.github.com>
2025-01-08 17:49:03 +0100

    update store router

--- src/server/router/store_router.ts
+++ src/server/router/store_router.ts
@@ -3,36 +3,139 @@ import { Store } from "@server";
 import { require } from "reliq";
 
 export function StoreRouter(_store: Store): Router {
-    return Router()
-        .get("/products", async (__, rs) => {
+    /** @constructor */ {
+        return Router()
+        .get("/store/products", async (__, rs) => {
             try {
-                rs.send({
-                    products: await _store.products()
-                });
+                rs.send({ products: await _store.products() });
                 return;
             }
             catch (e) {
                 console.error(e);
-                rs.send(e);
+                rs.send({ e });
                 return;
             }
         })
-        .get("/products-by-name", async (rq, rs) => {
+        .get("/store/products-by-name", async (rq, rs) => {
             try {
                 let { name } = rq.body;
-                let match: boolean =
+                if (!(
                     name !== null
                     && name !== undefined
                     && typeof name === "string"
-                    && name.trim().length !== 0;
-                require(match, "STORE_ROUTER.ERR_INVALID_REQUEST");
-                rs.send((await _store.productsByName(name)));
+                )) {
+                    rs.send({ message: "STORE_ROUTER.ERR_INVALID_REQUEST" });
+                    return;
+                }
+                rs.send({ products: await _store.products(name) });
                 return;
             }
             catch (e) {
                 console.error(e);
-                rs.send(e);
+                rs.send({ e });
                 return;
             }
-        });
+        })
+        .post("/store/set-stock", async (rq, rs) => {
+            try {
+                let { password, name, amount } = rq.body;
+                if (!(
+                    password !== null
+                    && password !== undefined
+                    && typeof password === "string"
+                    && _hasPermission(password)
+                    && name !== null
+                    && name !== undefined
+                    && typeof name === "string"
+                    && amount !== null
+                    && amount !== undefined
+                    && typeof amount === "number"
+                    && amount >= 0
+                    && amount <= Number.MAX_SAFE_INTEGER
+                    && Number.isSafeInteger(amount)
+                )) {
+                    rs.send({ message: "STORE_ROUTER.ERR_INVALID_REQUEST "});
+                    return;
+                }
+                await _store.setStock(name, BigInt(amount));
+                rs.send({ message: "OK" });
+                return;
+            }
+            catch (e) {
+                console.error(e);
+                rs.send({ e });
+                return;
+            }
+        })
+        .post("/store/increase-stock", async (rq, rs) => {
+            try {
+                let { password, name, amount } = rq.body;
+                if (!(
+                    password !== null
+                    && password !== undefined
+                    && typeof password === "string"
+                    && _hasPermission(password)
+                    && name !== null
+                    && name !== undefined
+                    && typeof name === "string"
+                    && amount !== null
+                    && amount !== undefined
+                    && typeof amount === "number"
+                    && amount >= 0
+                    && amount <= Number.MAX_SAFE_INTEGER
+                    && Number.isSafeInteger(amount)
+                )) {
+                    rs.send({ message: "STORE_ROUTER.ERR_INVALID_REQUEST" });
+                    return;
+                }
+                await _store.increaseStock(name, BigInt(amount));
+                rs.send({ message: "OK" });
+                return;
+            }
+            catch (e) {
+                console.error(e);
+                rs.send({ e });
+                return;
+            }
+        })
+        .post("/store/decrease-stock", async (rq, rs) => {
+            try {
+                let { password, name, amount } = rq.body;
+                if (!(
+                    password !== null
+                    && password !== undefined
+                    && typeof password === "string"
+                    && _hasPermission(password)
+                    && name !== null
+                    && name !== undefined
+                    && typeof name === "string"
+                    && amount !== null
+                    && amount !== undefined
+                    && typeof amount === "number"
+                    && amount >= 0
+                    && amount <= Number.MAX_SAFE_INTEGER
+                    && Number.isSafeInteger(amount)
+                )) {
+                    rs.send({ message: "STORE_ROUTER.ERR_INVALID_REQUEST" });
+                    return;
+                }
+                await _store.decreaseStock(name, BigInt(amount));
+                rs.send({ message: "OK" });
+                return;
+            }
+            catch (e) {
+                console.error(e);
+                rs.send({ e });
+                return;
+            }
+        })
+    }
+
+    function _hasPermission(password: string): void {
+        let correctPassword: string | undefined = process.env?.["ADMIN_PASSWORD"];
+        require(correctPassword !== undefined, "ADMIN_ROUTER.ERR_ADMIN_PASSWORD_REQUIRED");
+        require(correctPassword.trim().length !== 0, "ADMIN_ROUTER.ERR_INVALID_ADMIN_PASSWORD");
+        require(correctPassword === password, "ADMIN_ROUTER.ERR_INCORRECT_PASSWORD");
+        return;
+    }
 }
